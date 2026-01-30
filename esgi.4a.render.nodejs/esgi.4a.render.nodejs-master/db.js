817683ed871642f5be6b56b87cae73546647147c
PRADELLE Nathan <nathan.pradelle77@gmail.com>
2026-01-30 14:55:25 +0100

    final commit for env

--- esgi.4a.render.nodejs/esgi.4a.render.nodejs-master/db.js
+++ esgi.4a.render.nodejs/esgi.4a.render.nodejs-master/db.js
@@ -1,7 +1,22 @@
 const { Sequelize } = require('sequelize')
 
 // Database
-const sequelize = new Sequelize(DATABASE_URL);
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL, // TODO: database connection string
+  {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    define: {
+      createdAt: 'added',
+      updatedAt: 'updated',
+    }
+  },
+)
 
 // Database initialization
 async function initDatabase() {
